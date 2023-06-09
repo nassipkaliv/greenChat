3cb3186ccb3b88b871cf64c4fa4c9ca09ecd98bf
nassipkaliv <nassipkaliv@gmail.com>
2023-06-09 21:33:28 +0500

    layout!

---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { useLocation } from 'react-router-dom';
+
+const Sidebar = () => {
+  const location = useLocation().pathname === '/' ? true : false;
+  return (
+    <>
+      {location && <div id='Sidebar' className='text-dark border-r border-borderColor'>Sidebar</div>}
+    </>
+  )
+}
+
+export default Sidebar;
