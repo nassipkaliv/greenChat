1200a182c993705171dfcbbda28e6a9fa042f590
nassipkaliv <nassipkaliv@gmail.com>
2023-06-09 21:55:36 +0500

    axios

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const AppWrapper = (props: React.ComponentProps<any>) => {
   return (
     <section id="AppWrapper"
-    className="w-full my-10 mx-4 sm:mx-16 lg:mx-20 lg:my-8 flex text-[#415266] shadow-sm"
-    {...props}
-    >something{props.children}
+    className="w-full my-10 mx-4 sm:mx-16 lg:mx-20 lg:my-8 flex text-mainTextColor"
+    {...props}>
+      {props.children}
     </section>
   )
 }
