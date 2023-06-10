a65dd086c62bfaa6cb6428093a75b5013e2bd386
nassipkaliv <nassipkaliv@gmail.com>
2023-06-10 23:09:32 +0500

    edit

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,24 +1,16 @@
-import { lazy, useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthContext } from "../context/authContext";
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "../layout/DefaultLayout";
-import { getCookie } from "../utils/cookieHandler";
 
 const Index = lazy(() => import("../pages/Index"));
 const Login = lazy(() => import("../pages/Login"));
 
-
-
-
 const AppRoutes = () => {
-  const { isLogin }: any = useAuthContext();
-
-
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={isLogin ? <Index /> : <Navigate to='/login' />} />
-        <Route path="/login" element={!isLogin ? <Login /> : <Navigate to='/' />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
