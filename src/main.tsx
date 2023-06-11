1418ca38b0c3c3806106bb55bbf7bb3cc36a0697
nassipkaliv <nassipkaliv@gmail.com>
2023-06-11 15:47:47 +0500

    styled components

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ChatPage from "./pages/chat";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <ChatPage />
+  </React.StrictMode>
+);
+
