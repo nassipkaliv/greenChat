47dbc271b1b84af48697c6bc2c9c0bab7b0eb56a
nassipkaliv <nassipkaliv@gmail.com>
2023-06-12 14:00:20 +0500

    splash hook && style

---
+++ src/pages/chat/hooks/useAppLoad.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+type AppLoadProps = { 
+  incrementProgressValue?: number;
+  progressInterval?: number;
+  successLoadedTimeout?: number;
+  stoppedProgressValue?: number;
+  isManualProgressCompleted?: boolean;
+};
+
+export default function useAppLoad(props?: AppLoadProps) {
+  const {
+    progressInterval = 500,
+    incrementProgressValue = 10,
+    successLoadedTimeout = 3000,
+    stoppedProgressValue = 70,
+    isManualProgressCompleted = true,
+  } = props ?? {};
+
+  const [progress, setProgress] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if(prev >= stoppedProgressValue) {
+          clearInterval(interval);
+        }
+
+        return prev + incrementProgressValue;
+      });
+    }, progressInterval);
+
+    return () => {
+      if(interval) {
+        clearInterval(interval);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    let timeout: any;
+    if (progress >= stoppedProgressValue && isManualProgressCompleted) {
+      timeout = setTimeout(() => {
+        setIsLoaded(true);
+      }, successLoadedTimeout);
+    }
+
+    return () => {
+      if(timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [progress]);
+
+  const done = () => {
+    if(!isManualProgressCompleted) {
+      setIsLoaded(true);
+    }
+  };
+
+  return { progress, isLoaded, done };
+}
