2f075db5d59a9d5590ec61b6f24d8eba0a63231e
nassipkaliv <nassipkaliv@gmail.com>
2023-06-12 13:35:36 +0500

    Splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import ChatPage from "./pages/chat/ChatPage";
+import SplashPage from "./pages/splash/SplashPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ChatPage />
+    {/* <ChatPage /> */}
+    <SplashPage />
   </React.StrictMode>
 );
 
---
+++ src/pages/splash/SplashPage.tsx
@@ -0,0 +1,64 @@
+import { styled } from "styled-components";
+import Icon from "../../common/components/icons";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f0f2f5;
+  color: #41525d;
+`;
+
+const WhatsAppIcon = styled(Icon)`
+  fill: #9d9d9d;
+  fill: #bebebe;
+  width: 65px;
+  height: 65px;
+`;
+
+const LoadingContainer = styled.div`
+  width: 30%;
+  margin: 40px 0;
+  border: 1px solid #bebebe40;
+  background-color: #bebebe40;
+
+  hr {
+    border: 1px solid green;
+    width: 10%:
+  }
+`;
+
+const Title = styled.p`
+  font-size: 1.3rem;
+`;
+
+const Subtitle = styled.div`
+  margin-top: 10px;
+  color: #777878;
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+
+  span {
+    margin-left: 3px;
+  }
+`;
+
+export default function SplashPage() {
+  return (
+    <Container>
+      <WhatsAppIcon id="whatsapp" />
+      <LoadingContainer>
+        <hr/>
+      </LoadingContainer>
+      <Title>WhatsApp</Title>
+      <Subtitle>
+        <Icon id="lock" />
+        <span>End-to-end encrypted</span>
+        </Subtitle>
+    </Container>
+  );
+}
