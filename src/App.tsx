e84c4940e17bb693885dda0ad68b11adec148948
nassipkaliv <nassipkaliv@gmail.com>
2023-06-12 14:35:17 +0500

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
+import AppRoutes from "./routes/AppRoutes";
 import useAppLoad from "./pages/chat/hooks/useAppLoad";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-
-const ChatPage = React.lazy(() => import("./pages/chat/ChatPage"));
 const SplashPage = React.lazy(() => import("./pages/splash/SplashPage"));
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<ChatPage />} />)
-);
 
 export default function App() {
   const { isLoaded, progress } = useAppLoad();
 
   if (!isLoaded) return <SplashPage progress={progress} />;
-  return <RouterProvider router={router} />;
+  return <AppRoutes />;
 }
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+
+const ChatPage = React.lazy(() => import("../pages/chat/ChatPage"));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<ChatPage />} />)
+);
+
+export default function AppRoutes() {
+  return <RouterProvider router={router} />
+}
