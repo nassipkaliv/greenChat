210f7586cac0d92b8ddac0cf39127c6b69d2e480
nassipkaliv <nassipkaliv@gmail.com>
2023-06-14 00:15:34 +0500

    custom hook for navigation

--- src/pages/chat/components/chat-room/ChatRoom.tsx
+++ src/pages/chat/components/chat-room/ChatRoom.tsx
@@ -6,11 +6,13 @@ import Icon from "../../../../common/components/icons";
 import MessagesList from "./messages-list/MessagesList";
 import SearchSection from "./search-section/SearchSection";
 import { Container, Body, Background, FooterContainer, ScrollButton } from "./styles";
+import ChatLayout from "../../layouts/ChatLayout";
 import useChatRoom from "../../hooks/useChatRoom";
+import useNavigateToChat from "../../hooks/useNavigateToChat";
 
-export default function ChatRoom() {
-  
+export default function ChatRoomPage() {
   const {
+    activeInbox,
     handleMenuOpen,
     handleShowIcon,
     isProfileOpen,
@@ -21,37 +23,44 @@ export default function ChatRoom() {
     setShouldScrollToBottom,
     shouldScrollToBottom,
   } = useChatRoom();
-
+  useNavigateToChat(activeInbox);
+  
   return (
-    <Container>
-      <Body>
-        <Background />
-        <Header
-          title="Nassipkali Yernur"
-          subTitle="TOPG Online"
-          image="/public/images/avatar.jpg"
-          onSearchClick={() => handleMenuOpen("search")}
-          onProfileClick={() => handleMenuOpen("profile")}
-        />
-        <MessagesList 
-          onShowBottomIcon={handleShowIcon}
-          shouldScrollToBottom={shouldScrollToBottom}
-        />
-        <FooterContainer>
-          {isShowIcon && (
-            <ScrollButton onClick={() => setShouldScrollToBottom(true)}>
-              <Icon id="downArrow" />
-            </ScrollButton>
-          )}
-          <Footer />
-        </FooterContainer>
-      </Body>
-      <Sidebar title="Search" isOpen={isSearchOpen} onClose={() => setIsSearchOpen(false)}>
-        <SearchSection />
-      </Sidebar>
-      <Sidebar title="Contact Info" isOpen={isProfileOpen} onClose={() => setIsProfileOpen(false)}>
-        <ProfileSection />
-      </Sidebar>
-    </Container>
+    <ChatLayout>
+      <Container>
+        <Body>
+          <Background />
+          <Header
+            title={activeInbox?.name ?? ""}
+            image={activeInbox?.image ?? ""}
+            subTitle={activeInbox?.isOnline ? "Online" : ""}
+            onSearchClick={() => handleMenuOpen("search")}
+            onProfileClick={() => handleMenuOpen("profile")}
+          />
+          <MessagesList
+            onShowBottomIcon={handleShowIcon}
+            shouldScrollToBottom={shouldScrollToBottom}
+          />
+          <FooterContainer>
+            {isShowIcon && (
+              <ScrollButton onClick={() => setShouldScrollToBottom(true)}>
+                <Icon id="downArrow" />
+              </ScrollButton>
+            )}
+            <Footer />
+          </FooterContainer>
+        </Body>
+        <Sidebar title="Search" isOpen={isSearchOpen} onClose={() => setIsSearchOpen(false)}>
+          <SearchSection />
+        </Sidebar>
+        <Sidebar
+          title="Contact Info"
+          isOpen={isProfileOpen}
+          onClose={() => setIsProfileOpen(false)}
+        >
+          <ProfileSection name={activeInbox?.name ?? ""} image={activeInbox?.image ?? ""} />
+        </Sidebar>
+      </Container>
+    </ChatLayout>
   );
 }
