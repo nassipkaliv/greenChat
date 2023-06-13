77a02989c5caed9ad2eb366e762d9168d7899101
nassipkaliv <nassipkaliv@gmail.com>
2023-06-14 00:43:43 +0500

    added light theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { MainPageLoader } from "./common/components/loader";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./common/theme/DefaultTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Suspense fallback={<MainPageLoader />}>
-      <App />
+      <ThemeProvider theme={lightTheme}>
+        <App />
+      </ThemeProvider>
     </Suspense>
   </React.StrictMode>
 );
